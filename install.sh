c2b7caf71cb7c1c7f44270413cd3612527be1f9c
alkali <white@Bunker.YoRHa.org>
2025-10-19 21:38:09 +0800

    Mod install tool

--- install.sh
+++ install.sh
@@ -6,30 +6,10 @@
 ################################################################
 
 KERNEL_VER=$(uname -r)
-SRC_DIR=/usr/src
-LINUX_SRC=$SRC_DIR/linux-source-$(echo "$KERNEL_VER" | awk -F. '{print $1"."$2}')
-LINUX_SRC_FILE=$LINUX_SRC.tar.xz
 FASTLINQ_DIR=$(dirname "$(readlink -f "$0")")
 N_PROC=$(nproc)
 
 ################################################################
-
-if [ ! -d "$LINUX_SRC" ] && [ -f "$LINUX_SRC_FILE" ]; then
-    sudo tar -xf $LINUX_SRC_FILE -C $SRC_DIR
-elif [ ! -d "$LINUX_SRC" ]; then
-    sudo apt install linux-source
-    if [ ! -f "$LINUX_SRC_FILE" ]; then
-        exit 1
-    fi
-    sudo tar -xf $LINUX_SRC_FILE -C $SRC_DIR
-    if [ ! -d "$LINUX_SRC" ]; then
-        exit 1
-    fi
-fi
-
-unset SRC_DIR
-unset LINUX_SRC
-unset LINUX_SRC_FILE
 
 if [ ! -d "/lib/modules/$KERNEL_VER/build" ]; then
     sudo apt install linux-headers-$KERNEL_VER -y
